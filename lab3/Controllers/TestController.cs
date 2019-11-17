b15156104aa194264256991868dd7de5b195ba5c
Aviel Cohen <38301471+AvielCo@users.noreply.github.com>
2019-11-17 17:33:56 +0200

    Add project files.

---
+++ lab3/Controllers/TestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab3.Models;
+
+namespace lab3.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Load()
+        {
+            Test test = new Test()
+            {
+                FirstName = "Aviel",
+                LastName = "Cohen",
+                Email = "Mail",
+                Phone = "Phone"
+            };
+
+            return View(test);
+        }
+
+        public ActionResult Submit()
+        {
+            if (ModelState.IsValid)
+            {
+                Test test = new Test()
+                {
+                    FirstName = Request.Form["FirstName"],
+                    LastName = Request.Form["LastName"],
+                    Email = Request.Form["Email"],
+                    Phone = Request.Form["Phone"]
+                };
+
+                return View("Load", test);
+            }
+            return View("Submit");
+        }
+
+    }
+}
---
+++ lab3/Models/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab3.Models
+{
+    public class Test
+    {
+        [Required]
+        [StringLength(20,MinimumLength =2,ErrorMessage = "Name must be between 2 to 20")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Last must be between 2 to 20")]
+        public string LastName { get; set; }
+        [RegularExpression("^[0][5][0-4][(0-9)*]{10}$")]
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
